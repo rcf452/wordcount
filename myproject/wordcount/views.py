c3a8ee7312f608458bae89e789ac6933e922b105
Kim Yeonji <rcf452@gmail.com>
2019-06-08 15:26:44 +0900

    commit

--- myproject/wordcount/views.py
+++ myproject/wordcount/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request, 'wordcount/home.html')
+def index(request):
+    return render(request, 'wordcount/index.html')
 
 def about(request):
     return render(request, 'wordcount/about.html')
