208ca2a00df5c8dcbf00abb49890f830bc4157e4
Kim Yeonji <rcf452@gmail.com>
2019-06-08 15:08:36 +0900

    commit

---
+++ myproject/wordcount/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'wordcount/home.html')
+
+def about(request):
+    return render(request, 'wordcount/about.html')
+
+def count(request):
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+    word_dictionary={}
+
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else :
+            word_dictionary[word] = 1
+    return render(request, 'wordcount/count.html', {'fulltext': full_text, 'total':len(word_list), 'dictionary':word_dictionary.items()})
+
+def alphabet(request):
+    full_text = request.GET['fulltext']
+
+    alphabet_dictionary={}
+ 
+    for alphabet in full_text[:]:
+        if(alphabet != ' ' and alphabet!="." and alphabet != ","):
+            if alphabet in alphabet_dictionary:
+                alphabet_dictionary[alphabet] += 1
+            else :
+                alphabet_dictionary[alphabet] = 1
+    return render(request, 'wordcount/alphabet.html', {'fulltext': full_text, 'total':len(alphabet), 'dictionary':alphabet_dictionary.items()})
